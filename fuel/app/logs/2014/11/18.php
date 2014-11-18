0a97be318e06ac0bb60941dd8c9d292dbc065301
Nicolas Le Gall <nicolaslegall34@gmail.com>
2014-11-18 21:04:13 +0100

    Initial installation

---
+++ fuel/app/logs/2014/11/18.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2014-11-18 20:58:35 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
