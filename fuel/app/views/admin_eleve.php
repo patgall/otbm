084182a85f603b054fa1714ea999d17b96aae897
Nicolas Le Gall <nicolaslegall34@gmail.com>
2014-11-21 12:06:11 +0100

    view vor student admin

---
+++ fuel/app/views/admin_eleve.php
@@ -0,0 +1,22 @@
+<div class="row">
+    <div class="col-md-4">
+        <?php echo Html::anchor('util/compte', 'Mon compte', array('class' => 'btn btn-default', 'style' => 'padding-top: 4px;')); ?>
+    </div>
+    <div class="col-md-4">
+        <?php echo Html::anchor('util/compte', 'Mon compte', array('class' => 'btn btn-default', 'style' => 'padding-top: 4px;')); ?>
+    </div>
+    <div class="col-md-4">
+        <?php echo Html::anchor('util/compte', 'Mon compte', array('class' => 'btn btn-default', 'style' => 'padding-top: 4px;')); ?>
+    </div>
+</div>
+<hr>
+<div class="row">
+    <div class="col-md-4">
+        <ul class="nav nav-pills nav-stacked">
+            <li><a href="#">Informations</a></li>
+            <li><a href="#">Préférences</a></li>
+            <li><a href="#">Exercices</a></li>
+        </ul>
+    </div>
+    <div class="col-md-8">TEST</div>
+</div>
